d23908a7b4ce70b71952b74087ff4450c4864034
specdrake <akhooda.hooda@gmail.com>
2020-04-24 00:00:24 +0530

    adds new program

---
+++ p31.c
@@ -0,0 +1,23 @@
+//Program to pass and return pointer to function hence calculate average of an array.
+
+#include <stdio.h>
+#include <stdlib.h>
+double calcAv(int n, int* ar);
+int main()
+{
+  int n;
+  printf("No. of elements in the array : "); 
+  scanf("%d", &n);
+  int* ar = (int *)malloc(n * sizeof(int));
+  for(int i = 0; i < n; i++)
+    scanf("%d", ar + i);
+  double av = calcAv(n, ar); 
+  printf("The average of the array is %.2f\n", av);
+}
+double calcAv(int n, int* ar)
+{
+  int sum = 0;
+  for(int i = 0; i < n; i++)
+    sum += *(ar + i);
+  return sum/(double)n;
+}
