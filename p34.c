aef273c3535c73c14b5073864518b674ae68c547
specdrake <akhooda.hooda@gmail.com>
2020-04-23 22:32:14 +0530

    added program 34

--- p34.c
+++ p34.c
@@ -17,7 +17,8 @@ int main()
   scanf("%d", &emp1.age);
   printf("Enter your salary : ");
   scanf("%lf", &emp1.salary);
-  fprintf(fil, "Name : %sAge: %i\nSalary: %.2lf\n",emp1.name, emp1.age, emp1.salary);
+  fprintf(fil, "Name : %sAge: %i\nSalary: %.2lf\n",emp1.name, emp1.age, 
+      emp1.salary);
   fclose(fil);
 
  
